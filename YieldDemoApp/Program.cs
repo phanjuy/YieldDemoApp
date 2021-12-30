c66cd2ee71439ef52a00383fb3777929ad83c01d
Duy Phan <dpha@netcompany.com>
2021-12-30 02:07:25 +0100

    Iterate IEnumerable collection

--- YieldDemoApp/Program.cs
+++ YieldDemoApp/Program.cs
@@ -16,11 +16,42 @@ namespace YieldDemoApp
                 Console.WriteLine($"Read {p.FirstName} {p.LastName}");
             }
 
-            var primeNumbers30000 = Generators.GetPrimeNumbers().Take(30000).ToList(); // Dont do this
+            var primeNumbers30000 = Generators.GetPrimeNumbers().Take(30000);
 
             foreach (var prime in primeNumbers30000)
             {
                 Console.WriteLine(prime);
+            }
+
+            var primeNumbers = Generators.GetPrimeNumbers();
+
+            using var iterator = primeNumbers.GetEnumerator();
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (iterator.MoveNext())
+                {
+                    Console.WriteLine(iterator.Current);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("Waiting for user input");
+            Console.ReadLine();
+
+            for (int i = 0; i < 15; i++)
+            {
+                if (iterator.MoveNext())
+                {
+                    Console.WriteLine(iterator.Current);
+                }
+                else
+                {
+                    break;
+                }
             }
 
             Console.WriteLine("End of the App");
